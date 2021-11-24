a1a3e00934df7ce5968368220225ffa2dc16748c
Santiago Ruiz <santiago.sruiz@mercadolibre.com.co>
2021-11-24 18:24:52 -0500

    JWT authentication

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,16 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PeliculaController;
+use App\Http\Controllers\UserController;
 
 
-Route::resource('/peliculas', PeliculaController::class);
+
+
+Route::post('register', [UserController::class, 'register']);
+Route::post('login', [UserController::class, 'authenticate']);
+
+
+Route::group(['middleware' => ['jwt.verify']], function() {
+    Route::resource('/peliculas', PeliculaController::class);
+    Route::post('/me',[UserController::class, 'getAuthenticatedUser']);
+});
